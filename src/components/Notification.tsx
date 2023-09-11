74b5834d96892928912ae7b5f3515e10fb1af014
arief sangga <arif.sangga@gmail.com>
2023-09-11 20:52:21 +0700

    first commit

---
+++ src/components/Notification.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Notification = () => {
+  return (
+    <div className='h-12 bg-red-500 text-white px-4 flex items-center justify-center text-center text-sm md:text-base cursor-pointer'>Free delivery semua orders over 50 ribu, Order makanan Sekarang juga!</div>
+  )
+}
+
+export default Notification
